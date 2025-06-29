c5db366f7b075462ef0e17569cb144b1f58d1b0c
Sauce <telmobelasauce@ua.pt>
2025-06-29 20:06:57 +0100

    use of a package for alpaca

--- main.py
+++ main.py
@@ -1,45 +1,69 @@
-from webscraper import *
-import math
+import sys
+import time
+from utils import * 
+
+import requests
+import configparser
+import pandas as pd
+from datetime import datetime, timedelta
+import numpy as np
+
+from alpaca.trading.client import TradingClient
+from alpaca.data.timeframe import TimeFrame, TimeFrameUnit
+from alpaca.data.historical.corporate_actions import CorporateActionsClient
+from alpaca.data.historical.stock import StockHistoricalDataClient
+from alpaca.trading.stream import TradingStream
+from alpaca.data.live.stock import StockDataStream
+
+from alpaca.data.requests import * 
+from alpaca.trading.requests import * 
+from alpaca.trading.enums import *
+
+# Load from config.ini
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+api_key = config["keys"]["api_key"]
+secret_key = config["keys"]["api_secret"]
+paper=True
+
+stock_historical_data_client = StockHistoricalDataClient(api_key, secret_key)
+trade_client = TradingClient(api_key=api_key, secret_key=secret_key, paper=paper)
+stock_data_stream_client = StockDataStream(api_key, secret_key)
 
 def main():
 
-    #TODO
-    # Make the stop loss ajust with time going up
-    # Edit order to change stop loss
-
     symbol = "TSLA"
-    timeframe = "1min"
+    timeframe = TimeFrame(amount = 1, unit = TimeFrameUnit.Hour)
     feed="iex"
-    start_date = (datetime.now() - timedelta(days=1)).date()
+    days = 1 
 
-    data_frame = pd.DataFrame()
+    data_request = StockBarsRequest(
+        symbol_or_symbols = [symbol],
+        timeframe=TimeFrame(amount = 1, unit = TimeFrameUnit.Minute), 
+        start = (datetime.now() - timedelta(days=days)).date(),  
+        feed=feed,
+    )
 
-    data_frame = get_bars_data(data_frame, symbol, timeframe, start_date, feed)
-    close_price = data_frame.iloc[-1]["c"]
+    data_frame = stock_historical_data_client.get_stock_bars(data_request).df
+    close_price = data_frame.iloc[-1]["close"]
 
     qty = math.floor((close_price / get_account()["cash"]) * 10) / 10
     entry_price = close_price * 1.1
     take_profit = close_price * 2
     stop_loss = close_price * 1
 
-    #Order payload
-    payload = {
-        "symbol": symbol,
-        "qty": str(qty),
-        "side": "buy",
-        "type": "stop_limit",
-        "time_in_force": "gtc",
-        "stop_price": str(entry_price),     
-        "order_class": "bracket",
-        "take_profit": {
-            "limit_price": str(take_profit) 
-        },
-        "stop_loss": {
-            "stop_price": str(stop_loss),
-        }
-    }
-
-    order_stock(payload)
+    req = StopLimitOrderRequest(
+                symbol = symbol,
+                qty = qty,
+                side = OrderSide.BUY,
+                time_in_force = TimeInForce.DAY,
+                stop_price = entry_price, 
+                order_class = OrderClass.BRACKET,
+                take_profit = TakeProfitRequest(limit_price=take_profit),
+                stop_loss = StopLossRequest(stop_price=stop_loss)
+    )
+    res = trade_client.submit_order(req)
 
 if __name__ == "__main__":
     main()
